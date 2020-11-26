d32c94114edc034df2514e2d1af1451cbe8b3a33
田渭刚 <976044879@qq.com>
2020-11-26 16:01:59 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -20,8 +21,21 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader ,User $user)
     {
+        // 第一种方法
+        //$file = $request->file('avatar');
+       // dd($request->avatar);
+
+       $data = $request->all();
+
+       if($request->avatar){
+           $result = $uploader->save($request->avatar,'avatars',$user->id);
+           if($result){
+               $data['avatar'] = $result['path'];
+           }
+       }
+
         $user->update($request->all());
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功！');
     }
